99dd7c8b5cc02f5d579cf3ea8e32fd39d9cfeefb
andersson <anderssonq@gmail.com>
2018-12-30 18:57:12 -0400

    adelanto 30, proyectos fotos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import M from 'materialize-css'
-import imgsliderdg from '../images/projects/dg.jpg'
+import imgsliderdg from '../images/projects/dg1.jpg'
+import imgslider11 from '../images/projects/11.jpg'
+import imgslider22 from '../images/projects/22.jpg'
 export default class Projects extends Component {
 
     componentDidMount = () =>{
@@ -18,25 +20,26 @@ export default class Projects extends Component {
                 <ul className="slides">
                 <li>
                     <img src={imgsliderdg}/> 
-                    <div className="caption center-align">
-                    <h3>This is our big Tagline!</h3>
-                    <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://lorempixel.com/580/250/nature/2"/> 
                     <div className="caption left-align">
-                    <h3>Left Aligned Caption</h3>
-                    <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>DG Giros & Remesas</h3>
+                    <h5 className="light grey-text text-lighten-3">Aplicacion orientadas a remesas y cambios.</h5>
                     </div>
                 </li>
                 <li>
-                    <img src="https://lorempixel.com/580/250/nature/3"/> 
+                    <img src={imgslider11}/> 
                     <div className="caption right-align">
-                    <h3>Right Aligned Caption</h3>
-                    <h5 className="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                    <h3>Interbeverage</h3>
+                    <h5 className="light grey-text text-lighten-3">Pagina Estatica Wordpress, en proyecto en colaboracion con Smarketing Japan.</h5>
                     </div>
                 </li>
+                <li>
+                    <img src={imgslider22}/> 
+                    <div className="caption center-align">
+                    <h3>Uni-Glass</h3>
+                    <h5 className="light grey-text text-lighten-3">Pagina Estatica Wordpress, en proyecto en colaboracion con Smarketing Japan.</h5>
+                    </div>
+                </li>
+
                 </ul>
             </div>
       </div>
